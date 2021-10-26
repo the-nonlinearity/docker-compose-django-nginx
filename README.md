c26e487878fafd7e2c19c3145193c99a4079bbff
Salomon van Huyssteen <salomonvh@gmail.com>
2021-10-26 16:53:24 +0400

    Serve staticfiles for django.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # docker-compose-django-nginx
+
+## Run Django Application
+
+    docker-compose up
+
+## Migrations
+
+Make migrations
+
+    docker-compose build
+    docker-compose run --rm backend /bin/bash -c "./manage.py migrate"
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_nginx',
+        'USER': 'django_nginx_role',
+        'PASSWORD': 'django_nginx_password',
+        'HOST': 'postgres',  # <-- IMPORTANT: same name as docker-compose service!
+        'PORT': '5432',
     }
 }
 
@@ -123,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# as declared in NginX conf, it must match /opt/services/djangoapp/static/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# do the same for media files, it must match /opt/services/djangoapp/media/
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
